8b9b3c4e65184d22d37a42f6231e791983798e77
zhangyajie <z_yajie@sina.com>
2020-05-27 10:14:31 +0800

    添加祖册页面

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -9,10 +9,10 @@
           <span class="icon-bar"></span>
         </button>
 
-        <a href="/" class="navbar-brand">
-          <span class="title">{{ logo.title }}</span>
-          <img :src="logo.src" :alt="logo.title">
-        </a>
+		<router-link to="/" class="navbar-brand">
+		  <span class="title">{{ logo.title }}</span>
+		  <img :src="logo.src" :alt="logo.title">
+		</router-link>
       </div>
 
       <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
