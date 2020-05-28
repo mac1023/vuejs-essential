625b068d211171348b0126d9e4b7511b5cc2b5ea
zhangyajie <z_yajie@sina.com>
2020-05-28 14:11:13 +0800

    完成用户退出登录界面

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import './components'
 // 引入 store/index.js 的默认值
 import store from './store'
 
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+//使用插件
+Vue.use(VueSweetalert2)
+
 Vue.config.productionTip = false
 
 new Vue({
