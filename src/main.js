c60ef10ebd8556f8a136a987a12d7728681c6962
zhangyajie <z_yajie@sina.com>
2020-05-28 14:59:31 +0800

    完成用户 个人中心

--- src/main.js
+++ src/main.js
@@ -3,18 +3,19 @@ import App from './App'
 import router from './router'
 import './directives'
 import './components'
-// 引入 store/index.js 的默认值
 import store from './store'
-
 import VueSweetalert2 from './plugins/vue-sweetalert2'
-//使用插件
+// 引入插件
+import Message from './plugins/message'
+
 Vue.use(VueSweetalert2)
+// 使用插件
+Vue.use(Message)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  // 注入 store
   store,
   render: h => h(App),
 }).$mount('#app')
