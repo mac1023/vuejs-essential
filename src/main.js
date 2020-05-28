6c7d0a4bd61253ad409bc434615e18ded53f9ce1
zhangyajie <z_yajie@sina.com>
2020-05-28 11:20:20 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
 import './directives'
 import './components'
+// 引入 store/index.js 的默认值
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-	router,
+  router,
+  // 注入 store
+  store,
   render: h => h(App),
 }).$mount('#app')
